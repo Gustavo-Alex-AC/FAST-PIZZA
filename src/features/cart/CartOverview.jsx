a63067ad9ca69ba6b3cd2166b62ce45dc9cae9e0
Gustavo Chitangua <gchitangua@gmail.com>
2025-06-04 11:28:17 +0100

    yes_opening_cart_autenticado

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -6,10 +6,11 @@ import { formatCurrency } from "../../utils/helpers";
 function CartOverview() {
   const totalCartQuantity = useSelector(getTotalCartQuantity);
   const totalCartPrice = useSelector(getTotalCartPrice);
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
 
   console.log(totalCartQuantity, totalCartPrice);
 
-  if (!totalCartQuantity) return null;
+  if (!totalCartQuantity || !isAuthenticated) return null;
 
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
